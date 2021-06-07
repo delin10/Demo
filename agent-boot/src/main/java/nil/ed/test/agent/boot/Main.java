ba2324bdc95a8784d03112bed27242075ea5a89c
lidelin <lidelin@kkworld.com>
2021-06-08 07:57:44 +0800

    doc: add doc.

--- agent-boot/src/main/java/nil/ed/test/agent/boot/Main.java
+++ agent-boot/src/main/java/nil/ed/test/agent/boot/Main.java
@@ -2,6 +2,11 @@ package nil.ed.test.agent.boot;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * boot parameter:
+ * -javaagent:/Users/admin/delin/private-workspace/Demo/agent-boot/src/main/resources/lib/agent-jar-1.0-SNAPSHOT.jar
+ * -javaagent:jarpath[=options]
+ */
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
